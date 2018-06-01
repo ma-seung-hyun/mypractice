4f473ecf0949344607124d29b831666aee9896ad
dreamless64 <dreamless64@gmail.com>
2018-06-01 18:37:12 +0900

    진행

--- 신한실버케어/js/script.js
+++ 신한실버케어/js/script.js
@@ -1,4 +1,4 @@
-(function(){
+// (function(){
 
 var computedStyleX = function () {
   if (!window.getComputedStyle) {
@@ -15,16 +15,42 @@ var style = function(el,pr){
 var gnb = document.getElementsByClassName('gnb')[0],
     gnbwrap = gnb.getElementsByClassName('gnbwrap')[0];
 
+var indexDimdVideo = function(){
+  var dimd = document.getElementsByClassName('all-dimd')[0],
+      bg = dimd.getElementsByClassName('dimd-bg')[0],
+      video = dimd.getElementsByClassName('iframewrap')[0];
+
+  var clickBtn = document.querySelector('.video-btn button.btn');
+
+  // console.log(dimd,bg,video,clickBtn);
+
+  return {
+    position : function(){
+      video.style.top =
+      (window.innerHeight/2) - (style(video)['height'].split('px')[0]/2) + 'px';
+      video.style.left =
+      (window.innerWidth/2) - (style(video)['width'].split('px')[0]/2) + 'px';
+    },
+    open : function(){
+      clickBtn.addEventListener('click', function(){
+        dimd.classList.add('dimdon');
+      });
+    },
+    close : function(){
+      dimd.addEventListener('click', function(){
+        dimd.classList.remove('dimdon');
+      });
+    }
+  }
+}
+
 var readyPos = function(){
-  gnbwrap.style.marginTop = (style(gnb)['height'].split('px')[0]/2) - (style(gnbwrap)['height'].split('px')[0]/2) + 'px'
 };
 
 window.addEventListener('DOMContentLoaded', function(){
-  // computedStyleX();
-  // readyPos();
+  computedStyleX();
 });
 window.addEventListener('resize', function(){
-  // readyPos();
 });
 
-})();
+// })();
